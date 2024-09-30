e421b7bbbefcc8da267c63f71eb7f79fad41d66c
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-09-30 18:14:39 +0300

    commit

--- cypress/e2e/test_shop_full-screen_spec.js
+++ cypress/e2e/test_shop_full-screen_spec.js
@@ -26,34 +26,69 @@ describe('Shop Page (Full Screen)', function () {
     //     });     
     // });
 
-    it('should filter products by availability', function () {
-        let counter = 0;
+    // it('should filter products by availability', function () {
+
+    //     _exeption.exeption();
+    //     viewPort.fullScreen();
+    //     urlVisit.shopPage();
+    //     _shop.shopSelector().click();
+
+    //     _shop.availabilitySelector().click();
+    //     // in stock
+    //     _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true }); // примуове виконання
+    //     cy.wait(1000);
+
+    //     // reset
+    //     _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true });
+
+    //     // out of stock
+    //     _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
+    //     cy.wait(1000);            
+    //     // reset
+    //     _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
+
+    //     //all availability
+    //     _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true }); // примуове виконання
+    //     _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
+    //     cy.wait(1000);
+    //     // reset
+    //     _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true }); // примуове виконання
+    //     _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
+
+    // });
+
+    it('should add a product to the cart in the Shop section', function () {
+        
         _exeption.exeption();
         viewPort.fullScreen();
         urlVisit.shopPage();
         _shop.shopSelector().click();
 
-        _shop.availabilitySelector().click();
-        // in stock
-        _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true }); // примуове виконання
-        cy.wait(1000);
 
-        // reset
-        _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true });
+        const shopProducts = [
+            '#CardLink-template--14828910739534__product-grid-6960027533390', //contains("Telnyx Classic Hat")
+            '#CardLink-template--14828910739534__product-grid-7428845666382', //contains("Telnyx International Travel Adapter")
+            '#CardLink-template--14828910739534__product-grid-7428954161230', //contains("Telnyx Portable Wireless Power Charger")
+            '#CardLink-template--14828910739534__product-grid-6915266183246', //contains("Telnyx Pullover Hoodie")
+            '#CardLink-template--14828910739534__product-grid-6738575687758', //contains("Telnyx SIM Card for IoT and M2M (10PK)")
+            '#CardLink-template--14828910739534__product-grid-6751953256526', //contains("Telnyx SIM Card for IoT and M2M (1PK)")
+            '#CardLink-template--14828910739534__product-grid-6761603727438', //contains("Telnyx SIM Card for IoT and M2M (3PK)")
+          ];
+        
+          shopProducts.forEach(product => {
+            cy.get(product).should('be.visible').click();
 
-        // out of stock
-        _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
-        cy.wait(1000);            
-        // reset
-        _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
+                cy.contains('Add to cart ').click(); // add to cart
+                cy.get('#CartDrawer > div.drawer__inner > div.drawer__header > button').click(); // close cart
+                cy.wait(1000);
+                cy.go('back');
+        });
+    })
 
-        //all availability
-        _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true }); // примуове виконання
-        _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
-        cy.wait(1000);
-        // reset
-        _shop.inStockAvailabilitySelector().should('be.visible').click({ force: true }); // примуове виконання
-        _shop.outStockAvailabilitySelector().should('be.visible').click({ force: true });
-cy.log('counter ' + counter);
-    });
+
+
+    // cy.get('#CardLink-template--14828910739534__product-grid-6960027533390').click();
+    // cy.get('button[name="add"]').click(); // add to cart
+    // cy.get('#CartDrawer > div.drawer__inner > div.drawer__header > button').click(); // close cart
+
 });
